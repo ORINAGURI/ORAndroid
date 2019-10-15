ae9999ac7d4959a59ce56a2b73cd4f0b33512c13
신예지 <gjsld1@naver.com>
2019-10-15 13:15:20 +0900

    orinaguri complete

--- app/src/main/java/com/team/orinaguri/MainActivity.kt
+++ app/src/main/java/com/team/orinaguri/MainActivity.kt
@@ -2,11 +2,22 @@ package com.team.orinaguri
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        mapImg.setOnClickListener {
+            if(miseImg.visibility== View.VISIBLE) miseImg.visibility=View.INVISIBLE
+            else miseImg.visibility =  View.VISIBLE
+        }
     }
+
+
 }
