43b7e22609844539a9673401145af7e12168a3e2
신예지 <gjsld1@naver.com>
2019-11-11 20:43:48 +0900

    orinaguri google map

---
+++ app/src/main/java/com/team/orinaguri/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.team.orinaguri
+
+
+import android.os.Bundle
+import android.support.annotation.RequiresPermission
+import android.support.v7.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity: AppCompatActivity(), OnMapReadyCallback {
+    private var mMap:GoogleMap = null!!
+
+    override fun onCreate(savedInstanceState:Bundle?) {
+        super.onCreate(savedInstanceState)
+        /*
+        setContentView(R.layout.activity_main)
+
+        val mapFragment = getSupportFragmentManager().findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        */
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        mMap = googleMap!!
+        val SEOUL = LatLng(37.56, 126.97)
+        val markerOptions = MarkerOptions()
+        markerOptions.position(SEOUL)
+        markerOptions.title("서울")
+        markerOptions.snippet("한국의 수도")
+        mMap.addMarker(markerOptions)
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL))
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(10F))
+    }
+
+}
